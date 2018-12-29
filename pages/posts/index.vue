c0727c90de658b204c40d0299b303b6e9d6f000e
wanghao8080 <117663444@qq.com>
2018-12-29 14:10:58 +0800

    载入页面初始数据（asyncData）

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
     <h1 class="display-1">List</h1>
+    <div
+      v-for="post in posts"
+      :key="post.id"
+    >
+      {{ post.title }}
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const posts = await $axios.$get('http://localhost:3333/posts')
+    return { posts }
+  }
+}
+</script>
